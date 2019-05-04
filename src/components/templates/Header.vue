0937ce6a2e580dcac2bc156d5cccaaaa5f1297a9
Jared <jared.martinez@logicalposition.com>
2019-05-03 17:28:51 -0700

    Trying to fix slow down issue

--- src/components/templates/Header.vue
+++ src/components/templates/Header.vue
@@ -4,6 +4,7 @@
             <appNav />
             <b-row class="text-center" id="nonHero">
                 <h1 class="display-4 w-100">{{title}}</h1>
+                <appParticles :value=75 :value_area=750 />
             </b-row>
         </b-container>
     </header>
@@ -14,7 +15,7 @@
     import appNav from './Nav.vue';
 
     // Particles
-    require('particles.js');
+    import appParticles from './Particles.vue';
 
     export default {
         name: 'appHeader',
@@ -22,120 +23,8 @@
             title: String
         },
         components: {
-            appNav
-        },
-        mounted() {
-            // Particles JS
-            particlesJS('nonHero', {
-                    "particles": {
-                        "number": {
-                            "value": 200,
-                            "density": {
-                                "enable": true,
-                                "value_area": 400
-                            }
-                        },
-                        "color": {
-                            "value": "#ffffff"
-                        },
-                        "shape": {
-                            "type": "circle",
-                            "stroke": {
-                                "width": 0,
-                                "color": "#000000"
-                            },
-                            "polygon": {
-                                "nb_sides": 5
-                            },
-                            "image": {
-                                "src": '',
-                                "width": 100,
-                                "height": 100
-                            }
-                        },
-                        "opacity": {
-                            "value": 0.5524033491425908,
-                            "random": true,
-                            "anim": {
-                                "enable": true,
-                                "speed": 1,
-                                "opacity_min": 0,
-                                "sync": false
-                            }
-                        },
-                        "size": {
-                            "value": 3,
-                            "random": true,
-                            "anim": {
-                                "enable": false,
-                                "speed": 4,
-                                "size_min": 0.3,
-                                "sync": false
-                            }
-                        },
-                        "line_linked": {
-                            "enable": true,
-                            "distance": 64,
-                            "color": "#ffffff",
-                            "opacity": 0.4,
-                            "width": 1
-                        },
-                        "move": {
-                            "enable": true,
-                            "speed": 1,
-                            "direction": "random",
-                            "random": true,
-                            "straight": false,
-                            "out_mode": "bounce",
-                            "bounce": false,
-                            "attract": {
-                                "enable": false,
-                                "rotateX": 600,
-                                "rotateY": 600
-                            }
-                        }
-                    },
-                    "interactivity": {
-                        "detect_on": "canvas",
-                        "events": {
-                        "onhover": {
-                            "enable": false,
-                            "mode": "repulse"
-                        },
-                        "onclick": {
-                            "enable": false,
-                            "mode": "repulse"
-                        },
-                        "resize": true
-                        },
-                        "modes": {
-                        "grab": {
-                            "distance": 400,
-                            "line_linked": {
-                            "opacity": 1
-                            }
-                        },
-                        "bubble": {
-                            "distance": 250,
-                            "size": 0,
-                            "duration": 2,
-                            "opacity": 0,
-                            "speed": 3
-                        },
-                        "repulse": {
-                            "distance": 100,
-                            "duration": 0.4
-                        },
-                        "push": {
-                            "particles_nb": 4
-                        },
-                        "remove": {
-                            "particles_nb": 2
-                        }
-                        }
-                    },
-                    "retina_detect": false
-                });
+            appNav,
+            appParticles
         }
     }
 </script>
--- src/components/templates/HeaderHome.vue
+++ src/components/templates/HeaderHome.vue
@@ -6,6 +6,7 @@
                     <h1 class="w-100 display-4">Martinez Designs</h1>
                     <img src="https://martinezdesigns.net/images/favicon.png" alt="logo">
                     <h3 class="lead w-100">I am a <span class="tagline"></span></h3>
+                    <appParticles :value=150 :value_area=1000 />
                 </b-row>
         </b-container>
     </header>
@@ -15,16 +16,17 @@
     // Nav
     import appNav from './Nav.vue';
 
+    // Particles
+    import appParticles from './Particles.vue';
+
     // Typed JS
     import Typed from 'typed.js';
     
-    // Particles JS
-    require('particles.js');
-
     export default {
         name: 'appHeader',
         components: {
-            appNav
+            appNav,
+            appParticles
         },
         mounted () {
             // Typed JS
@@ -35,120 +37,7 @@
                 backSpeed: 40,
                 backDelay: 1000
             }
-
             new Typed(".tagline", options);
-
-            // Particles JS
-            particlesJS('hero', {
-                    "particles": {
-                        "number": {
-                            "value": 200,
-                            "density": {
-                                "enable": true,
-                                "value_area": 750
-                            }
-                        },
-                        "color": {
-                            "value": "#ffffff"
-                        },
-                        "shape": {
-                            "type": "circle",
-                            "stroke": {
-                                "width": 0,
-                                "color": "#000000"
-                            },
-                            "polygon": {
-                                "nb_sides": 5
-                            },
-                            "image": {
-                                "src": "img/github.svg",
-                                "width": 100,
-                                "height": 100
-                            }
-                        },
-                        "opacity": {
-                            "value": 0.5524033491425908,
-                            "random": true,
-                            "anim": {
-                                "enable": true,
-                                "speed": 1,
-                                "opacity_min": 0,
-                                "sync": false
-                            }
-                        },
-                        "size": {
-                            "value": 3,
-                            "random": true,
-                            "anim": {
-                                "enable": false,
-                                "speed": 4,
-                                "size_min": 0.3,
-                                "sync": false
-                            }
-                        },
-                        "line_linked": {
-                            "enable": true,
-                            "distance": 64,
-                            "color": "#ffffff",
-                            "opacity": 0.5,
-                            "width": 1
-                        },
-                        "move": {
-                            "enable": true,
-                            "speed": 2,
-                            "direction": "random",
-                            "random": true,
-                            "straight": false,
-                            "out_mode": "out",
-                            "bounce": false,
-                            "attract": {
-                                "enable": false,
-                                "rotateX": 600,
-                                "rotateY": 600
-                            }
-                        }
-                    },
-                    "interactivity": {
-                        "detect_on": "canvas",
-                        "events": {
-                        "onhover": {
-                            "enable": false,
-                            "mode": "repulse"
-                        },
-                        "onclick": {
-                            "enable": false,
-                            "mode": "repulse"
-                        },
-                        "resize": true
-                        },
-                        "modes": {
-                        "grab": {
-                            "distance": 400,
-                            "line_linked": {
-                            "opacity": 1
-                            }
-                        },
-                        "bubble": {
-                            "distance": 250,
-                            "size": 0,
-                            "duration": 2,
-                            "opacity": 0,
-                            "speed": 3
-                        },
-                        "repulse": {
-                            "distance": 100,
-                            "duration": 0.4
-                        },
-                        "push": {
-                            "particles_nb": 4
-                        },
-                        "remove": {
-                            "particles_nb": 2
-                        }
-                        }
-                    },
-                    "retina_detect": false
-                });
         }
     }
 </script>
