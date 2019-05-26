648ddfc77909391c18d333e2e469b6466b60a0a9
Jared <jared.martinez@logicalposition.com>
2019-05-26 11:58:40 -0700

    Adding additional project

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,7 +3,7 @@
         <appHeader title="Projects" />
         <main>
             <b-container fluid>
-                <b-row class="text-center text-md-left" id="myfit">
+                <b-row class="text-center text-md-left" id="aceinthehole">
                     <h2 class="display-4">Ace In The Hole Multisport</h2>
                 </b-row>
                 <b-row class='p-3 p-md-4'>
@@ -16,6 +16,7 @@
                                 Ace In The Hole Multisport
                             </b-list-group-item>
                             <b-list-group-item href="https://jaredmartinez.webhostingforstudents.com/cas222/">Made January 2019</b-list-group-item>
+                            <b-list-group-item href="https://github.com/SoftTaco97/aceinthehole" class="text-info">Browse the code</b-list-group-item>
                         </b-list-group>
                     </b-col>
                     <b-col lg="5" offset-lg="1">
@@ -46,6 +47,47 @@
                                 </li>
                             </ul>
                         </p>
+                    </b-col>
+                </b-row>
+                <b-row class="text-center text-md-right" id="giphy">
+                    <h2 class="display-4">Giphy Search</h2>
+                </b-row>
+                <b-row class='p-3 p-md-4'>
+                    <b-col class="d-lg-none">
+                        <a href="img/giphy-full.png" data-lightbox="giphy-search">
+                            <img src="img/giphy.png">
+                        </a>
+                        <b-list-group class="mt-5">
+                            <b-list-group-item href="/giphy-search/" class="text-info">
+                                Giphy Search
+                            </b-list-group-item>
+                            <b-list-group-item href="/giphy-search/">Made July 2018</b-list-group-item>
+                            <b-list-group-item href="https://github.com/v1st/Giphy-Search" class="text-info">Browse the code</b-list-group-item>
+                        </b-list-group>
+                    </b-col>
+                    <b-col lg="5" offset-lg="1">
+                        <h3 class="display-5 border-bottom text-center p-3">The Brief</h3>
+                        <p class="lead mt-4">
+                            Giphy Search is a web application utilizing the Giphy Developer API.
+                        </p>
+                        <p class="lead">
+                            The project was made in collaboration with a remote web developer based in Florida.<br>
+                        </p>
+                        <p class="lead">
+                            This web application utitlizes JavaScript, jQuery, HTML and CSS for its functionality.
+                        </p>
+                    </b-col>
+                    <b-col lg="4" offset-lg="1" class="d-none d-lg-block">
+                        <a href="img/giphy-full.png" data-lightbox="giphy-search">
+                            <img src="img/giphy.png">
+                        </a>
+                        <b-list-group class="mt-5">
+                            <b-list-group-item href="/giphy-search/" class="text-info">
+                                Giphy Search
+                            </b-list-group-item>
+                            <b-list-group-item href="/giphy-search/">Made July 2018</b-list-group-item>
+                            <b-list-group-item href="https://github.com/v1st/Giphy-Search" class="text-info">Browse the code</b-list-group-item>
+                        </b-list-group>
                     </b-col>
                 </b-row>
             </b-container>
